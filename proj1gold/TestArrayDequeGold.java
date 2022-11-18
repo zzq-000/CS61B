e722f0e16ae9b2de732248aaca821b47fa60ddaa
zzq <2663219537@qq.com>
2022-11-18 19:08:32 +0800

    project1 gold finished

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class TestArrayDequeGold {
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class TestArrayDequeGold {
+    public static final int TIMES = 100;
+    public static final double A1 = 0.25;
+    public static final double A2 = 0.5;
+    public static final double A3 = 0.75;
+    public static final double A4 = 1;
+    @Test
+    public void testArrayDequeGold() {
+        StudentArrayDeque<Integer> actual = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> expect = new ArrayDequeSolution<>();
+        String info = "";
+        for (int i = 0; i < TIMES; i++) {
+            double action = StdRandom.uniform();
+            if (action < A1) {
+                actual.addLast(i);
+                expect.addLast(i);
+                info += "addLast(" + i + ")\n";
+            } else if (action < A2) {
+                actual.addFirst(i);
+                expect.addFirst(i);
+                info += "addFirst(" + i + ")\n";
+            } else if (action < A3) {
+                if (!actual.isEmpty()) {
+                    Integer a = actual.removeFirst();
+                    Integer b = expect.removeFirst();
+                    info += "removeFirst()\n";
+                    assertEquals(info, a, b);
+                    info = "";
+                }
+
+            } else if (action < A4) {
+                if (!actual.isEmpty()) {
+                    Integer a = actual.removeLast();
+                    Integer b = expect.removeLast();
+                    info += "removeLast()\n";
+                    assertEquals(info, a, b);
+                    info = "";
+                }
+
+            }
+        }
+    }
+    public static void main(String[] args) {
+        jh61b.junit.TestRunner.runTests(TestArrayDequeGold.class);
+    }
 }
