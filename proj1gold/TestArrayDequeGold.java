0b920beb20156d8231a116dab89f8c8c9d0a244a
zzq <2663219537@qq.com>
2022-11-18 19:16:09 +0800

    project1 gold finished

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -8,38 +8,53 @@ public class TestArrayDequeGold {
     public static final double A4 = 1;
     @Test
     public void testArrayDequeGold() {
-        StudentArrayDeque<Integer> actual = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> expect = new ArrayDequeSolution<>();
-        String info = "";
-        for (int i = 0; i < TIMES; i++) {
-            double action = StdRandom.uniform();
-            if (action < A1) {
-                actual.addLast(i);
-                expect.addLast(i);
-                info += "addLast(" + i + ")\n";
-            } else if (action < A2) {
-                actual.addFirst(i);
-                expect.addFirst(i);
-                info += "addFirst(" + i + ")\n";
-            } else if (action < A3) {
-                if (!actual.isEmpty()) {
-                    Integer a = actual.removeFirst();
-                    Integer b = expect.removeFirst();
-                    info += "removeFirst()\n";
-                    assertEquals(info, a, b);
-                    info = "";
+        StudentArrayDeque<Integer> testArray = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> stdArray = new ArrayDequeSolution<>();
+        String log = "";
+        for (int i = 0; i < 1000; i++) {
+            if (stdArray.size() == 0) {
+                int addNumber = StdRandom.uniform(1000);
+                int headOrBack = StdRandom.uniform(2);
+                if (headOrBack == 0) {
+                    log = log + "addFirst(" + addNumber + ")\n";
+                    testArray.addFirst(addNumber);
+                    stdArray.addFirst(addNumber);
+                } else {
+                    log = log + "addLast(" + addNumber + ")\n";
+                    testArray.addLast(addNumber);
+                    stdArray.addLast(addNumber);
                 }
-
-            } else if (action < A4) {
-                if (!actual.isEmpty()) {
-                    Integer a = actual.removeLast();
-                    Integer b = expect.removeLast();
-                    info += "removeLast()\n";
-                    assertEquals(info, a, b);
-                    info = "";
+            } else {
+                int x = StdRandom.uniform(4);
+                int addNumber = StdRandom.uniform(1000);
+                Integer testremoveNumber = 1;
+                Integer stdremoveNumber = 1;
+                switch (x) {
+                    case 0:
+                        log = log + "addFirst(" + addNumber + ")\n";
+                        testArray.addFirst(addNumber);
+                        stdArray.addFirst(addNumber);
+                        break;
+                    case 1:
+                        log = log + "addLast(" + addNumber + ")\n";
+                        testArray.addLast(addNumber);
+                        stdArray.addLast(addNumber);
+                        break;
+                    case 2:
+                        log = log + "removeFirst()\n";
+                        testremoveNumber = testArray.removeFirst();
+                        stdremoveNumber = stdArray.removeFirst();
+                        break;
+                    case 3:
+                        log = log + "removeLast()\n";
+                        testremoveNumber = testArray.removeLast();
+                        stdremoveNumber = stdArray.removeLast();
+                        break;
+                    default:
                 }
-
+                assertEquals(log, stdremoveNumber, testremoveNumber);
             }
         }
+
     }
 }
