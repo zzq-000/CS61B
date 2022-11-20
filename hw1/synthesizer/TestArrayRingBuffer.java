87bdb7841cb738f4c7d9680a2d92f41e0e048133
zzq <2663219537@qq.com>
2022-11-20 19:30:36 +0800

    peek新增抛出异常

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -1,5 +1,9 @@
 package synthesizer;
+import edu.princeton.cs.algs4.In;
 import org.junit.Test;
+
+import java.util.Iterator;
+
 import static org.junit.Assert.*;
 
 /** Tests the ArrayRingBuffer class.
@@ -9,7 +13,39 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(10);
+        for (int i = 0; i < 10; i++) {
+            arb.enqueue(i);
+        }
+        for(int i = 0; i < 5; i++){
+            arb.dequeue();
+        }
+        for(int i = 0; i < 3; i++){
+            arb.enqueue(i);
+        }
+
+    }
+
+    @Test
+    public void testIter() {
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(10);
+        for (int i = 0; i < 10; i++) {
+            arb.enqueue(i);
+        }
+        for(int i = 0; i < 5; i++){
+            arb.dequeue();
+        }
+        for(int i = 0; i < 3; i++){
+            arb.enqueue(i);
+        }
+        for (Integer i: arb) {
+            System.out.println(i);
+        }
+//        Iterator<Integer> ite= arb.iterator();
+//        while (ite.hasNext()) {
+//            Integer t = ite.next();
+//            System.out.println(t);
+//        }
     }
 
     /** Calls tests for ArrayRingBuffer. */
